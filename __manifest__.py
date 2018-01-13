e80a59968cdd6a6d605abc0820230156051d7136
Toufiq <burningraven06@gmail.com>
2018-01-13 20:28:50 +0600

    use pages in form view

--- __manifest__.py
+++ __manifest__.py
@@ -12,7 +12,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Module Development',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
