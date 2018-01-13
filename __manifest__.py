8f17a0a0ec86e95627c8e90232f1d8dd01fb4a52
Toufiq <burningraven06@gmail.com>
2018-01-13 20:58:17 +0600

    seperate views in different xml, update manifest.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,31 +1,20 @@
 # -*- coding: utf-8 -*-
 {
     'name': "openacademy",
-
     'summary': "Summary",
-
     'description': "Description",
-
     'author': "Haha",
     'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
     'category': 'Module Development',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
     'depends': ['base'],
-
-    # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/openacademy.xml'
+        'views/openacademy_course.xml',
+       # 'views/openacademy_session.xml'
     ],
-    # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
