61b6f104883922d3f185485b7940de05df14e014
Toufiq <burningraven06@gmail.com>
2018-01-15 21:46:25 +0600

    update manifest.py, create board xml for dashboard

--- __manifest__.py
+++ __manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "openacademy",
+    'name': "OpenAcademy",
     'summary': "Summary",
     'description': "Description",
     'author': "Haha",
-    'website': "http://www.yourcompany.com",
+    'website': "http://www.the_name_of_your_website.com",
     'category': 'Module Development',
     'version': '0.1',
-    'depends': ['base', 'report'],
+    'depends': ['base', 'report', 'board'],
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
@@ -16,7 +16,8 @@
         'views/openacademy_session.xml',
         'views/partner.xml',
         'views/openacademy_session_workflow.xml',
-        'views/reports.xml'
+        'views/reports.xml',
+        'views/session_board.xml'
     ],
     'installable': True,
     'application': True
